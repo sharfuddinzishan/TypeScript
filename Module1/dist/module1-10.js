8d58cc874f1add1b48db49c27d911140c8b0b51b
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-11-10 21:09:04 +0600

    Union Intersaction

--- Module1/dist/module1-10.js
+++ Module1/dist/module1-10.js
@@ -1,10 +1,44 @@
 "use strict";
 /*
-    Union Intersection
+    Union
+    - If newObject type is from two object using union |,
+      then, newObject must have one of objects all properties
+
+    Intersection
+    - If newObject type is from two object using intersaction &,
+      then, newObject must have two of objects all properties
 */
-const dev1 = {
-    scope: "Designer"
-};
-const dev2 = {
-    scope: "API"
-};
+{
+    const dev1 = {
+        scope: "Designer"
+    };
+    const dev2 = {
+        scope: "API"
+    };
+    /*
+    const user1:SUT={
+        salary:1200 // Need designation as ST is Union of Student Teacher
+    }
+    */
+    const user1 = {
+        salary: 1200,
+        designation: "Principle"
+    };
+    const user2 = {
+        roll: 12,
+        designation: 'Teacher',
+        salary: 12000
+    };
+    const user3 = {
+        name: "Sharfuddin",
+        age: 29,
+        designation: "Teacher",
+        salary: 20000
+    };
+    const user4 = {
+        name: "Rakib",
+        age: 20,
+        roll: 10,
+        marks: 29,
+    };
+}
--- Module1/src/module1-10.ts
+++ Module1/src/module1-10.ts
@@ -1,7 +1,13 @@
 /*
-    Union Intersection
-*/
+    Union
+    - If newObject type is from two object using union |,
+      then, newObject must have one of objects all properties
 
+    Intersection
+    - If newObject type is from two object using intersaction &,
+      then, newObject must have two of objects all properties
+*/
+{
 // union |
 type gender='Male'|'Female'
 
@@ -22,5 +28,64 @@ const dev2:BackEnd={
     scope:"API"
 }
 
+/* Union Example With Object */
+
+type Person={
+    name:string,
+    age:number
+}
+type Student={
+    marks?:number,
+    roll:number
+}
+type Teacher={
+    designation:"Teacher"|"Principle",
+    salary:number
+}
+
+type SUT=Student|Teacher
+
+/*
+const user1:SUT={
+    salary:1200 // Need designation as ST is Union of Student Teacher
+}
+*/
+const user1:SUT={
+    salary:1200, // Need designation as ST is Union of Student Teacher
+    designation:"Principle"
+}
+
+type SIT=Student & Teacher
+const user2:SIT={
+    roll:12,
+    designation:'Teacher',
+    salary:12000
+}
+
+/* 
+   **************** Standard Code******************* 
+*/
+
+type PIT=Person & Teacher
+type PIS=Person & Student
+
+const user3:PIT={
+    name:"Sharfuddin",
+    age:29,
+    designation:"Teacher",
+    salary:20000
+}
+
+const user4:PIS={
+    name:"Rakib",
+    age:20,
+    roll:10,
+    marks:29,
+}
 
 
+
+
+
+
+}
