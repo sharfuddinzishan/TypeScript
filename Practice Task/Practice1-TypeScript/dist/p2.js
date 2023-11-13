95cfc480ad71cf90113fa2a1365840bac88085fe
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-11-13 18:53:43 +0600

    Task2

--- Practice Task/Practice1-TypeScript/dist/p2.js
+++ Practice Task/Practice1-TypeScript/dist/p2.js
@@ -17,14 +17,45 @@ into a tidy and structured object, showcasing its newfound form.
         ['age', 24],
         ['isAuth', false]
     ];
- 
-
-
-   
-
-
-
-
-
-
+    /*
+        function getProps(obj:object){
+            let formatObject= datas.reduce((final:any,current:any)=>{
+                final[current[0]]=current[1]
+                return final
+            },{});
+            return formatObject
+        }
+        console.log(getProps(datas));
+    */
+    /*
+    function getProps<T extends string,Q>(obj:Array<[T,Q]>){
+        let formatObject= datas.reduce((final:any,current:any)=>{
+            final[current[0]]=current[1]
+            return final
+        },{});
+        return formatObject
+    }
+    console.log(getProps(datas));
+    */
+    /*
+    function getProps<T extends string,Q>(obj:Array<[T,Q]>):Record<string,any>{
+        let formatObject= datas.reduce((final,current)=>{
+            final[current[0]]=current[1]
+            return final
+        },{} as Record<string,any>);
+        return formatObject
+    }
+    console.log(getProps(datas));
+    */
+    // Record<string,any> means {key:value} where key always string type.
+    // Record generates an object
+    function getProps(obj) {
+        let formatObject = obj.reduce((final, [key, value]) => {
+            final[key] = value;
+            return final;
+        }, {});
+        return formatObject;
+    }
+    console.log(getProps([['name', 'Zishan']]));
+    console.log(getProps(datas));
 }
